69b689f69db41a55b185400b830223ff0c992810
Dhruv <dhruv.sharma.017@gmail.com>
2018-11-14 20:52:43 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include <conio.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <process.h>
-#include <fstream>			
+#include <fstream>
 
 using namespace std;
 
@@ -15,16 +16,47 @@ public:
 	char name[100];
 	char uname[100];
 	char pass[100];
+	
+	account() {
+		name[0] = '\0';
+		uname[0] = '\0';
+		pass[0] = '\0';
+	}
+};
+
+class questc{
+public:
+	char quest[500];
+	char ans[100];
+	char tc[100];
+	
+	questc() {
+		quest[0] = '\0';
+		ans[0] = '\0';
+		tc[0] = '\0';
+	}
 };
 
 class stud : public account {
 public:
 	char clas[3];
+	char path[100];
+	int score;
+	
+	stud(){
+		clas[0] = '\0';
+		path[0] = '\0';
+		score = 0;
+	}
 };
 
 class tchr : public account {
 	public:
 	char clas[3];
+	
+	tchr(){
+		clas[0] = '\0';
+	}
 };
 
 int login();
@@ -32,14 +64,16 @@ int signup();
 int stud_win(stud stdtmp);
 int tchr_win(tchr tchtmp);
 int main_menu();
+int submit(char *tmpath, questc qtmp);
+int adquest(char *path);
 
 int main() {
 	main_menu();
-	cout << "Goodbye!";
+	cout << "Goodbye!\n\n\n";
 	return 0;
 }
 
-int main_menu(){
+int main_menu(){									//Main menu windo
 	char per;
 	cout << "\t /***********************************************************\\\n";
 	cout << "\t|    __       ___  __     __   __   __    __    __     ___    |\n";
@@ -68,122 +102,9 @@ int main_menu(){
 	return 0;
 }
 
-int signup(){
-	char mod;
-	sign:
-	cout << "Select:\n";
-	cout << "Student (s)\n";
-	cout << "Teacher (t)\n";
-	cout << ">> ";
-	cin >> mod;
-	//clrscr();
-	system("cls");
-	if(mod == 's') {							//Student Sign-up
-		stud stud1;
-		ofstream ofile("std.dat");
-		int count = 0;
-		char tmp;
-		char spass[100];
-		if(!ofile) {
-			cout << "Fopen error!";
-		}
-		char tpass[100];
-		cout << "Enter Real Name: ";
-		cin.ignore();
-		gets(stud1.name);
-		cout << "Enter Username: ";
-		gets(stud1.uname);
-		signpass:
-		cout << "Enter class (Eg:12A): ";
-		gets(stud1.clas);
-		cout << "Enter Password: ";
-		do{
-		tmp = getch();
-		if(tmp == '\b' && count > 0){
-			cout << "\b";
-			count--;
-		}
-		else if( tmp != '\b' && tmp != '\r'){
-			cout << '*';
-			stud1.pass[count] = tmp;
-			count++;
-		}
-	}while(tmp != '\r');
-	stud1.pass[count] = '\0';
-	cout << endl;
-	count = 0;
-	cout << "Confirm Password: ";
-	do{
-		tmp = getch();
-		if(tmp == '\b' && count > 0){
-			cout << "\b";
-			count--;
-		}
-		else if( tmp != '\b' && tmp != '\r'){
-			cout << '*';
-			spass[count] = tmp;
-			count++;
-		}
-	}while(tmp != '\r');
-	spass[count] = '\0';
-	cout << endl;
-	if(strcmp(spass, stud1.pass) == 0)
-		ofile.write((char*)&stud1, sizeof(stud1));
-	else {
-		cout << "Passwords not matching!\n";
-		goto signpass;
-	}
-		ofile.close();
-	}
-	else if(mod == 't') {						//Teacher Sign-up
-		tchr tchr1;
-		ofstream ofile("tchr.dat");
-		int count = 0;
-		char tmp;
+int signup(){}										//Sign up windo
 
-		if(!ofile) {
-			cout << "Fopen error!";
-		}
-		char tpass[100];
-		cout << "Enter Real Name: ";
-		gets(tchr1.name);
-		cout << "Enter Username: ";
-		gets(tchr1.uname);
-		cout << "Enter Password: ";
-		do{
-		tmp = getch();
-		if(tmp == '\b' && count > 0){
-			cout << "\b";
-			count--;
-		}
-		else if( tmp != '\b' && tmp != '\r'){
-			cout << '*';
-			tchr1.pass[count] = tmp;
-			count++;
-		}
-	}while(tmp != '\r');
-	tchr1.pass[count] = '\0';
-		cout << "Confirm Password: ";
-		gets(tpass);
-
-		ofile.write((char*)&tchr1, sizeof(tchr1));
-		ofile.close();
-	}
-	else {
-		cout << "Invalid Option\n";
-		goto sign;
-	}
-	//clrscr();
-	system("cls");
-	cout << "Successful!";
-	getch();
-	//clrscr();
-	system("cls");
-	login();
-	return 0;
-}
-
-int login() {
+int login() {										//Login Window
 	int count = 0, flag = 0;
 	stud stud1;
 	tchr tchr1;
@@ -256,12 +177,17 @@ int login() {
 	return 0;
 }
 
-int stud_win(stud stdtmp) {
+int stud_win(stud stdtmp) {						//Student Window
 	system("cls");
 	//clrscr();
 	cout << "Welcome, " << stdtmp.name << endl;
 	int count = 0;
-	char com = 'y';
+	char com = 'y', fpath[100];
+	questc squest;
+	snprintf(fpath, 100, "Assignments/%s.dat", stdtmp.clas);
+	ifstream ifile(fpath, ios::binary);
+	for(int cnt = 0; cnt <= stdtmp.score; ifile.read((char*)&squest, sizeof(squest))){}
+	ifile.close();
 	do {
 		if (count > 0) {
 			cout << "Press any key to continue...";
@@ -269,6 +195,7 @@ int stud_win(stud stdtmp) {
 			//clrscr();
 			system("cls");
 		}
+		cout << "Place the cpp file in " << stdtmp.path << " before submitting.\n";
 		cout << "\t\tMenu:\n";
 		cout << "Submit: (s)\n";
 		cout << "Back: (b)\n";
@@ -279,6 +206,7 @@ int stud_win(stud stdtmp) {
 		system("cls");
 		switch (com) {
 		case 's':
+			submit(stdtmp.path, squest);
 			break;
 		case 'b':
 			main_menu();
@@ -292,7 +220,7 @@ int stud_win(stud stdtmp) {
 	return 0;
 }
 
-int tchr_win(tchr tchtmp) {
+int tchr_win(tchr tchtmp) {						//Teacher Windo
 	system("cls");
 	//clrscr();
 	cout << "Welcome, " << tchtmp.name << endl;
@@ -307,6 +235,8 @@ int tchr_win(tchr tchtmp) {
 		}
 		cout << "\t\tMenu:\n";
 		cout << "Display Student Details: (d)\n";
+		cout << "Add question: (a)\n";
+		cout << "Remove question: (r)\n";
 		cout << "Back: (b)\n";
 		cout << "Exit: (e)\n";
 		cout << "Enter Command: ";
@@ -314,16 +244,55 @@ int tchr_win(tchr tchtmp) {
 		//clrscr();
 		system("cls");
 		switch (com) {
+		case 'a':
+			adquest(tchtmp.clas);
+			break;
 		case 'b':
 			main_menu();
 			break;
 		case 'd':
 			break;
-		case 'e':
+		case 'r':
 			return 0;
 			break;
 		}
 		count++;
 	} while (com != 'e');
+	return 0;
+}
+
+int submit(char *tmpath, questc qtmp) {						//Compiles and checks submitted progra
+	int res;
+	char stat[100];
+	snprintf(stat, 100, "g++ %s/main.cpp", tmpath);
+	res = system(stat);
+	if(res == 1) {
+		cout << "Error compiling.\n";
+		return -1;	
+	}
+	else {
+		questc questemp;
+		snprintf(stat, 100, "ans.txt << ./a.out << %s", questemp.tc);
+		cout << "Successful compilation!\n";
+		system(stat);
+		system("diff");
+		
+	}
+	return 0;
+}
+
+int adquest(char *path) {						//add questio
+	char fpath[100];
+	snprintf(fpath, 100, "Assignments/%s.dat", path);
+	ofstream ofile(fpath, ios::app | ios::binary);
+	questc qtmp;
+	cout << "Enter a question: ";
+	gets(qtmp.quest);
+	cout << "Enter the testcases: ";
+	gets(qtmp.tc);
+	cout << "Enter expected answer: ";
+	gets(qtmp.ans);
+	ofile.write((char*)&qtmp, sizeof(qtmp));
+	ofile.close();
 	return 0;
 }
